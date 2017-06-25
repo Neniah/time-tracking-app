6b915d383881c8e416ac7b9f7808d7b7ffb0df64
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-06-25 20:10:23 +0200

    adding editable and timer components

--- src/Components/TimersDashBoard.js
+++ src/Components/TimersDashBoard.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
+import EditableTimerList from './EditableTimerList';
 
 class TimersDashBoard extends Component{
   render(){
     return(
       <div className="col-md-6 col-md-offset-3">
         <div className="row text-center">
-          <span>Editable Timer List</span>
+          <EditableTimerList/>
           <span>ToggleableTimerForm isOpen={true}</span>
         </div>
       </div>
